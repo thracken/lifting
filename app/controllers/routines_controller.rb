6b56d49e2754124bc4c03066dab4f0fe206586f7
Jeff <jeffgreenland.1@gmail.com>
2016-10-20 15:15:55 -0700

    Added Routines index page.

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,11 +1,20 @@
 class RoutinesController < ApplicationController
   def new
+    @routine = Routine.new
   end
 
   def create
+    @routine = Routine.new(routine_params)
+    if @routine.save
+      flash[:success] = "Routine added!"
+      redirect_to routines_url
+    else
+      render 'new'
+    end
   end
 
   def index
+    @routines = Routine.all
   end
 
   def show
@@ -19,4 +28,9 @@ class RoutinesController < ApplicationController
 
   def destroy
   end
+
+  private
+    def routine_params
+      params.require(:routine).permit(:name)
+    end
 end
