4e4ff74e325c2f1ecfe6a559f41f5e26515c67fc
Jeff <jeffgreenland.1@gmail.com>
2016-10-21 14:32:49 -0700

    Added more tests to make sure only logged in users can see/edit routines.

--- test/integration/user_routines_test.rb
+++ test/integration/user_routines_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class UserRoutinesTest < ActionDispatch::IntegrationTest
-  test "require a logged in user to create a routine" do
+  def setup
+    @user = users(:jeff)
+  end
+
+  test "require a logged in user to see/create a routine" do
     get new_routine_url
     assert_redirected_to login_url
+    get routines_url
+    assert_redirected_to login_url
+  end
+
+  test "logged in users can see all routines, and create new routines" do
+    log_in_as @user
+    get new_routine_url
+    assert_template "routines/new"
+    get routines_url
+    assert_template "routines/index"
   end
 end
