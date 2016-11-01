b7445792392251aaf05016a8fad395ede7af8375
Jeff <jeffgreenland.1@gmail.com>
2016-11-01 14:59:46 -0700

    Started Exercise Groups controller, and added a new exercise group link on the Routines index page.

--- app/controllers/exercise_groups_controller.rb
+++ app/controllers/exercise_groups_controller.rb
@@ -1,4 +1,35 @@
 class ExerciseGroupsController < ApplicationController
-  def new    
+  def new
+    @exercise_group = ExerciseGroup.new
   end
+
+  def create
+    @routine = Routine.find(params[:routine_id])
+    @exercise_group = @Routine.exercise_groups.new(exercise_group_params)
+    if @exercise_group.save
+      flash[:success] = "Group Added!"
+      render routines_url
+    else
+      render "new"
+    end
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def exercise_group_params
+    end
 end
