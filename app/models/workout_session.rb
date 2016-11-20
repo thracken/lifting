07f29f7aa8d68320fa132a442b071f3f5f7e7f74
thracken <jeffgreenland.1@gmail.com>
2016-11-20 11:56:32 -0700

    Added instance method to Workout Sessions to build Workout Exercises.

--- app/models/workout_session.rb
+++ app/models/workout_session.rb
@@ -11,9 +11,9 @@ class WorkoutSession < ActiveRecord::Base
   end
 
   def build_workout_exercises(routine, group)
-    return if routine.class != Routine
+    return if routine.class != Routine || group.class != ExerciseGroup
     group.exercises.each do |exercise|
-      
+      self.workout_exercises.create(workout_session_id: self.id, exercise_id: exercise.id, sets_goal: exercise.sets_goal, reps_goal: exercise.reps_goal)
     end
   end
 end
