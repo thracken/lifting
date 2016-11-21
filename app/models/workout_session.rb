950a0cf06df2255dbefe05d02106830628d6404b
Jeff Greenland <jeffgreenland.1@gmail.com>
2016-11-20 19:40:38 -0700

    Added method to Workout Sessions to get Workout Group from last Workout Session, updated method to build Workout Exercises.

--- app/models/workout_session.rb
+++ app/models/workout_session.rb
@@ -2,16 +2,18 @@ class WorkoutSession < ActiveRecord::Base
   belongs_to :user
   has_many :workout_exercises
 
-  def get_current_routine
+  def get_active_routine
     Routine.find_by(active_status: 1)
   end
 
   def get_last_workout_group
-
+    ExerciseGroup.find(self.user.workout_sessions.last.exercise_group_id)
   end
 
   def build_workout_exercises(routine, group)
     return if routine.class != Routine || group.class != ExerciseGroup
+    self.exercise_group_id = group.id
+
     group.exercises.each do |exercise|
       self.workout_exercises.create(workout_session_id: self.id, exercise_id: exercise.id, sets_goal: exercise.sets_goal, reps_goal: exercise.reps_goal)
     end
