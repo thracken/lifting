62b682a16189a9c906e5015eded38f400f1eb0e2
Jeff <jeffgreenland.1@gmail.com>
2017-01-03 10:25:18 -0700

    Got Workout Sessions tests passing by fixing fixture associations.

--- test/models/workout_session_test.rb
+++ test/models/workout_session_test.rb
@@ -3,10 +3,11 @@ require 'test_helper'
 class WorkoutSessionTest < ActiveSupport::TestCase
   def setup
     @user = users(:jeff)
+    @session = WorkoutSession.new(date: Time.now, user_id: @user.id)
     @routine = routines(:phraks)
   end
 
-  test "get a routine" do
-    assert_equal @routine.name, "Phrak's Grey Skull LP"
+  test "get active routine" do
+    assert_equal @session.get_active_routine, @routine
   end
 end
