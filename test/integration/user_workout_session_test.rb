0dc4dbdd28e45a5b2406e5e7489b04243751a97c
Jeff <jeffgreenland.1@gmail.com>
2017-01-03 15:01:21 -0700

    Added edit view for Workout Sessions, and updated integration test to include it.

--- test/integration/user_workout_session_test.rb
+++ test/integration/user_workout_session_test.rb
@@ -9,7 +9,8 @@ class UserWorkoutSessionTest < ActionDispatch::IntegrationTest
     log_in_as @user
     get new_workout_session_url
     assert_difference "WorkoutSession.count", 1 do
-      post workout_sessions_url, workout_session: {date: Time.now, user_id: @user.id}
+      post_via_redirect workout_sessions_url, workout_session: {date: Time.now, user_id: @user.id}
     end
+    assert_template "workout_sessions/edit"
   end
 end
