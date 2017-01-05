9393a4ca05c1f19a30d69bc69e26122b0acfc440
Jeff <jeffgreenland.1@gmail.com>
2017-01-05 14:58:25 -0700

    Added test for method incrementing exercise weight.

--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ExerciseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @exercise = Exercise.create(name: "Bench Press", weight: 45, weight_increment: 2.5, reps_goal: 5, sets_goal: 3, time_between_sets: 90)
+  end
+
+  test "weight increments properly" do
+    assert_equal @exercise.increment_weight, 47.5
+  end
 end
