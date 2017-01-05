a75a50aef967d59e157a0b0af86f566116820e2f
Jeff <jeffgreenland.1@gmail.com>
2017-01-05 11:58:49 -0700

    Added increment_weight method to Exercises.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -6,4 +6,8 @@ class Exercise < ActiveRecord::Base
   validates :sets_goal, presence: true
 
   has_and_belongs_to_many :exercise_groups
+
+  def increment_weight
+    self.weight = self.weight + self.weight_increment
+  end
 end
